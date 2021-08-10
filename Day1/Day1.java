ab1cd3f19a0b0f0b184c8a3e8385eb4b629aba54
VIJAY KUMAR PATEL <58830874+vijay320@users.noreply.github.com>
2021-08-10 18:12:02 +0530

    Add files via upload

---
+++ Day1/Day1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Student{
+    int roll;
+    String name;
+    float marks;
+
+    void input(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Student Roll Number : ");
+        roll = sc.nextInt();
+        System.out.print("Enter Student Name : ");
+        name = sc.next();
+        System.out.print("Enter Student Roll Marks : ");
+        marks = sc.nextFloat();
+        sc.close();
+
+    }
+    void display(){
+        System.out.println("Student Detail is => ");
+        System.out.println("Roll Number : " +roll);
+        System.out.println("Name : " +name);
+        System.out.println("Marks : " +marks);
+
+    }
+}
+
+class Day1 {
+    public static void main(String[] args){
+        Student s = new Student();
+        s.input();
+        s.display();
+
+    }
+    
+}
