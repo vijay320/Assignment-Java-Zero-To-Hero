a8b7bf624f85a6bea2565443ef0aa8dbc44336ea
VIJAY KUMAR PATEL <58830874+vijay320@users.noreply.github.com>
2021-08-10 18:17:11 +0530

    Update Day1.java

--- Day1/Day1.java
+++ Day1/Day1.java
@@ -1,4 +1,17 @@
-//Write a program to which will store details of a student and print the details using the above class.
+/* 
+    Assignment:
+
+2. Define a class Student with following members:
+
+int roll, String name, float marks.
+
+input() to take input of the details
+
+display() to all details of a student.
+
+
+Write a program to which will store details of a student and print the details using the above class.
+*/
 
 import java.util.Scanner;
 
