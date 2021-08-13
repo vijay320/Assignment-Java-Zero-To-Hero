141aa3365796b6531bac6bc6e99207d917f3a859
VIJAY KUMAR PATEL <58830874+vijay320@users.noreply.github.com>
2021-08-13 23:28:15 +0530

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -8,9 +8,15 @@ Assignment:
 
 class Day2{
     public static void main(String[] args){
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        int sum = a + b;
-        System.out.println("Sum of args is : "+sum);
+        try {
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+    
+            int sum = a + b;
+            System.out.println("Sum of args is : "+sum);
+        } catch (Exception e) {
+            System.out.println("Enter Command Line Argument ");
+        }
+       
     }
 }
